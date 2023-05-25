d96e1247aaf6a33a92a197db180c510b9237a099
Santos-Mateus <matteussbt@gmail.com>
2023-05-25 20:30:38 -0300

    Cria mecanismo de paginação dos pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,39 @@
 
+const listaPokemon = document.getElementById('listaPokemon')
+const botaoCarregarMais = document.getElementById('botaoCarregarMais')
+const limit = 5;
+let offSet = 0;
 
-function converterPokemonParaLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="numero">#${pokemon.numero}</span>
-            <span class="nome">${pokemon.nome}</span>
-            
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-                <img src="${pokemon.imagem}" 
-                    alt=${pokemon.nome}>
-            </div>
-        </li>
-    `
+function carregarItensPokemons(offSet, limit){
+    function converterPokemonParaLi(pokemon) {
+        return 
+    }
+    
+    pokeApi.getPokemons(offSet, limit).then((pokemons = []) => {
+        const novoHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="numero">#${pokemon.numero}</span>
+                <span class="nome">${pokemon.nome}</span>
+                
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.imagem}" 
+                        alt=${pokemon.nome}>
+                </div>
+            </li>
+        `).join('')
+        listaPokemon.innerHTML += novoHtml
+    })  
 }
 
-const listaPokemon = document.getElementById('listaPokemon')
-
-pokeApi.getPokemons().then((pokemons = []) => {
-    const novoHtml = pokemons.map(converterPokemonParaLi).join('')
-    listaPokemon.innerHTML = novoHtml
-})   
+carregarItensPokemons(offSet, limit)
  
+botaoCarregarMais.addEventListener('click', () => {
+    offSet += limit
+    carregarItensPokemons(offSet, limit)
+})
 
 
 
